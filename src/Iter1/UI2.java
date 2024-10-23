00be9c619d5e05ab16ad7419a5581dba25ffd2de
rune2976 <runeclund@hotmail.com>
2024-10-23 22:56:32 +0200

    lavede lige lidt

--- src/Iter1/UI2.java
+++ src/Iter1/UI2.java
@@ -4,8 +4,46 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
+
+public class UI2 extends Application {
+
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Sej titel");
+        button = new Button();
+        button.setText("START!!!");
+
+        button.setOnAction(e ->{
+            System.out.println("Du klikkede på knappen");
+            System.out.println("Wow!");
+        });
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
+
+
+
+/*
 public class UI2 extends Application {
 
     @Override
@@ -19,4 +57,4 @@ public class UI2 extends Application {
     public static void main(String[] args){
         launch(args);
     }
-}
+}*/
