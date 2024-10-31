72b411b544561195e4bd36c2d85b3480e8b23b4e
Buskemor <130304167+Buskemor@users.noreply.github.com>
2024-10-31 22:38:39 +0100

    simplified code, added readme

--- src/database/DBConnector.java
+++ src/database/DBConnector.java
@@ -19,129 +19,85 @@ public class DBConnector {
         int locationId = 0;
 
         java.sql.Date date = new java.sql.Date(new Date().getTime());
-        Category category = Category.WATER;
-//        int categoryId = 0;
+        Category category = Category.WATER; // doesn't do anything yet
 
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
 
-//          insert into description table
-//            PreparedStatement preparedStatement = connection.prepareStatement(
-//            "INSERT INTO description (description) VALUES(?)");
-//            preparedStatement.setString(1, description);
-//            preparedStatement.execute();
-
-//          insert into locations table
-//            preparedStatement = connection.prepareStatement(
-//            "INSERT INTO locations (road, housenumber) VALUES(?, ?)");
-//            preparedStatement.setString(1, road);
-//            preparedStatement.setInt(2, houseNumber);
-//            preparedStatement.execute();
-
 //          insert into locations table
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM locations WHERE road = ? AND housenumber = ?");
+            "SELECT * FROM locations WHERE road = ? AND housenumber = ?");
             preparedStatement.setString(1, road);
             preparedStatement.setInt(2, houseNumber);
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                if(resultSet.getInt("location_id") == 0) {
-                    System.out.println(email + " does not match any emails in the citizens table.");
-                    preparedStatement = connection.prepareStatement(
-                            "INSERT INTO locations (road, housenumber) VALUES(?,?)");
-                    preparedStatement.setString(1, road);
-                    preparedStatement.setInt(2, houseNumber);
-                    preparedStatement.execute();
-                }
+
+            while(resultSet.next()) {
+                System.out.println("location exists as location_id: " + resultSet.getInt("location_id"));
                 locationId = resultSet.getInt("location_id");
             }
+
+//            if no location id matches, run this
             if(locationId == 0) {
                 System.out.println("location id: " + locationId + ", is 0");
-                // insert into citizens table
                 preparedStatement = connection.prepareStatement(
-                        "SELECT MAX(location_id) FROM locations");
-//                preparedStatement.setString(1, email);
-                ResultSet resultSet2 = preparedStatement.executeQuery();
+                "SELECT MAX(location_id) FROM locations");
+                resultSet = preparedStatement.executeQuery();
 
-                while(resultSet2.next()) {
-                    citizenId = resultSet2.getInt("MAX(location_id)") + 1;
+                while(resultSet.next()) {
+                    locationId = resultSet.getInt("MAX(location_id)") + 1;
                 }
-                System.out.println(locationId);
-
                 preparedStatement = connection.prepareStatement(
-                        "INSERT INTO locations (road, housenumber) VALUES(?,?)");
+                "INSERT INTO locations (road, housenumber) VALUES(?,?)");
                 preparedStatement.setString(1, road);
                 preparedStatement.setInt(2, houseNumber);
                 preparedStatement.execute();
             }
 
 
-
-
 //          insert into citizens table
             preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM citizens WHERE ? = EMAIL");
+            "SELECT * FROM citizens WHERE ? = EMAIL");
             preparedStatement.setString(1, email);
             resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                if(resultSet.getInt("citizen_id") == 0) {
-                    System.out.println(email + " does not match any emails in the citizens table.");
-                    preparedStatement = connection.prepareStatement(
-                            "INSERT INTO citizens (email) VALUES(?)");
-                    preparedStatement.setString(1, email);
-                    preparedStatement.execute();
-                }
+                System.out.println("location exists as citizen_id: " + resultSet.getInt("citizen_id"));
                 citizenId = resultSet.getInt("citizen_id");
             }
+
+//            if no citizen id matches, run this
             if(citizenId == 0) {
                 System.out.println("citizen id: " + citizenId + ", is 0");
-                // insert into citizens table
                 preparedStatement = connection.prepareStatement(
-                        "SELECT MAX(citizen_id) FROM citizens");
-//                preparedStatement.setString(1, email);
-                ResultSet resultSet2 = preparedStatement.executeQuery();
-
-                while(resultSet2.next()) {
-                    citizenId = resultSet2.getInt("MAX(citizen_id)") + 1;
+                "SELECT MAX(citizen_id) FROM citizens");
+                resultSet = preparedStatement.executeQuery();
+                while(resultSet.next()) {
+                    citizenId = resultSet.getInt("MAX(citizen_id)") + 1;
                 }
-//                System.out.println(citizenId);
-
                 preparedStatement = connection.prepareStatement(
-                        "INSERT INTO citizens (email) VALUES(?)");
+                "INSERT INTO citizens (email) VALUES(?)");
                 preparedStatement.setString(1, email);
                 preparedStatement.execute();
             }
 
 
-
 //          insert into issues table
             preparedStatement = connection.prepareStatement(
-                    "INSERT INTO issues (date, description, category_id, status_id, location_id, citizen_id)"
-                            +"VALUES(?, ?, ?, ?, ?, ?)");
-
-
-
+            "INSERT INTO issues (date, description, category_id, status_id, location_id, citizen_id)"
+               +"VALUES(?, ?, ?, ?, ?, ?)");
             preparedStatement.setDate(1, date); //date
-
             preparedStatement.setString(2,description); //description
-
             preparedStatement.setInt(3,5); //category
-
             preparedStatement.setInt(4, 1); //status (CONSTANT)
-
             preparedStatement.setInt(5,locationId); //location
-
             preparedStatement.setInt(6, citizenId); //citizenId
 
-            System.out.println(citizenId);
             preparedStatement.execute();
-            System.out.println("everything executed");
+            System.out.println("Issue inserted into issues table successfully.");
 
         } catch (SQLException e) {
             System.out.println(e);
         }
-
     }
 }
--- src/database/note.md
+++
@@ -1,3 +0,0 @@
-DBConnectorWIP is trying to make the code more managable by splitting it up
-
-DBConnector is the working prototype. PROTOTYPE. It's unreadable code, but it works.
